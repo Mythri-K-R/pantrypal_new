997c6762beb28b096b6725267d45c77960a9b135
Mythri-K-R <mythrikr88@gmail.com>
2026-02-16 01:29:37 +0530

    Updated DB config to use DATABASE_URL for Render

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,16 +1,14 @@
 ﻿const { Pool } = require('pg');
-require('dotenv').config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.connect()
-  .then(() => console.log('Database Connected Successfully'))
-  .catch(err => console.error('Database Connection Error:', err));
+  .then(() => console.log("Database Connected Successfully"))
+  .catch(err => console.error("Database Connection Error:", err));
 
 module.exports = pool;
